81ea6c0825ccd272a8b4838f09723330cd5f247b
Sidney-C <158471670+Sidney-C@users.noreply.github.com>
2025-02-24 16:18:57 +0000

    added new site

--- Client/src/components/portfolio.jsx
+++ Client/src/components/portfolio.jsx
@@ -6,6 +6,10 @@ function Portfolio() {
     return (
         <div>
             <h1>My Portfolio</h1><br></br>
+            <a href="https://orwellbot.vercel.app/" style={Styles.linkStyle}>
+                <h2>🔗Orwell Simulator</h2>
+            </a><br></br>
+            <p>This is a fun bot inspired by George Orwell. Upload any passage of writing, and it will read it. Then, it will give you detailed feedback based on the rules for good writing set out by Orwell in his essay "Politics and the English Language". The feedback includes a score out of five, an analysis of the strengths and weaknesses of your writing, and a rewritten version of the passage.</p><br></br>
             <a href="https://flashcards-frontend-lac.vercel.app/" style={Styles.linkStyle}><h2>🔗Language Flashcards</h2></a><br></br>
             <p>This is an educational full-stack CRUD app (Create, Read, Update, Delete) built using the PERN stack (PostgreSQL, Express.js, React.js, Node.js) of technologies. Designed to assist in language learning, and suitable for all ages and levels of fluency, the user can create, read, update, and delete bilingual flashcards. They can then test themselves on these flashcards, having to translate words from their native language to their target language, or vice versa. While being tested, the user is provided with helpful information, such as a running total of right and wrong answers, and corrections whenever they make a mistake. Please note that the above link launches the app in a read-only demo mode, with the ability to add, edit and delete flashcards disabled for security reasons. However, it is still possible to view the flashcards, and attempt translation tests.</p><br></br>
             <a href="https://github.com/Sidney-C/Vite-React-Cleaner" style={Styles.linkStyle}><h2>🔗React.js Project Helper</h2></a><br></br>
